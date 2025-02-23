f0ede4c5ebcbf41fe3014acf490750325bb82d59
99morini@gmail.com <99morini@gmail.com>
2025-02-23 12:54:32 +0100

    added also album with only mp3

--- coverArtExtractor.py
+++ coverArtExtractor.py
@@ -8,7 +8,7 @@ def scanFolder(path):
     for root, directories, files in os.walk(path, topdown=True):
         for name in files:
             pathTmp=str(os.path.join(root, name))
-            if(pathTmp.endswith(".m4a")):
+            if(pathTmp.endswith(".m4a") or pathTmp.endswith("mp3")):
                 os.chdir(root) # go to albyh
                 mycmd = "no | ffmpeg -i ./"+name.replace(" ",r'\ ')+" -an ./cover.jpg ;" # yes/no to pipe the overwrite to ffmpeg to replace the coverart whenever already present
                 subprocess.run(mycmd, shell=True) # shell=True to manage whitespace and some other characters
