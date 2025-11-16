1cb787fda15ed6c9c130561e4c0ab2d79f85b328
albertomorini <99morini@gmail.com>
2025-11-16 23:39:07 +0100

    flac included

--- glyrics.py
+++ glyrics.py
@@ -1,12 +1,13 @@
 import os
 import sys
-import lyricsgenius as genius
+import json
+import hashlib
 from tinytag import TinyTag
 from mutagen.mp4 import MP4
+from mutagen.flac import FLAC
 from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, USLT
-import hashlib
-import json
+import lyricsgenius as genius
 
 ## GENIUS STUFF
 APIkey_genius= open("./genius_APIKEY.txt","r").read()
@@ -16,17 +17,16 @@ genius.skip_non_songs = False #we search also the songs without lyrics (eg sound
 ########################################################################
 # UTILITY
 
-def serializeJSON(dir, filename, dataDictionary):
-	f_handler = open(dir+"/"+filename,"w",encoding="utf-8")
-	f_handler.write(json.dumps(dataDictionary))
-	f_handler.close()
+def serializeJSON(dir_path, filename, dataDictionary):
+	with open(os.path.join(dir_path, filename), "w", encoding="utf-8") as f:
+		json.dump(dataDictionary, f, ensure_ascii=False, indent=2)
 
 def readJson(path):
 	try:
-		jfile = open(path,"r")
-		return json.loads(jfile.read())
-	except Exception as e:
-		print("Exception: config file not exists/not a json")
+		with open(path, "r", encoding="utf-8") as f:
+			return json.load(f)
+	except Exception:
+		print(f"Warning: {path} does not exist or is not valid JSON")
 		return None
 
 def doMD5(str_value):
@@ -45,64 +45,52 @@ def searchLyrics(pathSong):
 	except Exception as e:
 		return None
 
-#store the lyrics to the M4A song
-def storeLyricsM4A(pathSong, lyrics):
+#store the lyrics into song tag
+def storeLyrics(song_path, song_lyrics_text):
+	dummy_ext=song_path[-4:].lower() #just the extension
 	try:
-		song = MP4(pathSong)
-		song["©lyr"] = lyrics
-		song.save()
-		return True
+		if(dummy_ext == ".m4a"):
+			song = MP4(song_path)
+			song["©lyr"] = song_lyrics_text
+			song.save()
+		elif(dummy_ext == ".mp3"):
+			song = ID3(song_path)
+			song["USLT::'eng'"] = (USLT(encoding=3, lang=u'eng', desc=u'desc', text=song_lyrics_text))
+			song.save()
+		elif(dummy_ext == ".flac"):
+			song = FLAC(song_path)
+			print(song)
+			song["LYRICS"] = song_lyrics_text
+			song.save()
+		return [True,dummy_ext]
 	except Exception as e:
-		return False
+		print(e)
+		return [False,None]
 
-#store the lyrics to the MP3 song
-def storeLyricsMP3(pathSong, lyrics):
-	try:
-		song = ID3(pathSong)
-		song["USLT::'eng'"] = (USLT(encoding=3, lang=u'eng', desc=u'desc', text=lyrics))
-		song.save()
-		return True
-	except Exception as e:
-		return False
 
-def storeLyricsFLAC(pathSong,lyricstxt):
+#DANGER: erase the lyrics of the songs
+def flushLyrics(song_path):
+	dummy_ext=song_path[-4:].lower() #just the extension
 	try:
-		song = MP4(pathSong)
-		song["LYRICS"] = lyricstxt
-		song.save()
-		return True
+		if(dummy_ext == ".m4a"):
+			song = MP4(song_path)
+			song.pop("©lyr")
+			song.save()
+		elif(dummy_ext == ".mp3"):
+			song = ID3(song_path)
+			song.pop("USLT::'eng'")
+			song.save()
+		elif(dummy_ext == ".flac"):
+			song = FLAC(song_path)
+			song.pop("LYRICS")
+			song.save()
+		return [True,dummy_ext]
 	except Exception as e:
-		return False
+		print(e)
+		return [False,None]
 
 ########################################################################
 ## LOGIC AND ALGORITHM
-
-#DANGER: erase the lyrics of the songs
-def flushLyrics(path):
-	x = str(input("Insert 'suRe' if you're really sure\n"))
-	if (x=='suRe'):
-		for root, directories, files in os.walk(path, topdown=True):
-			for name in files:
-				pathTmp=str(os.path.join(root, name))
-				try:
-					if(pathTmp.endswith(".m4a")):
-						song = MP4(pathTmp)
-						song.pop("©lyr")
-						song.save()
-						print("Removed lyrics for :",name)
-					elif(pathTmp.endswith(".flac")):
-						song = MP4(pathTmp)
-						song.pop("LYRICS")
-						song.save()
-						print("Removed lyrics for :",name)
-					elif(pathTmp.endswith(".mp3")):
-						song = ID3(i)
-						song.pop("USLT::'eng'")
-						song.save()
-						print("Removed lyrics for :",name)
-					
-				except Exception as e: ## whenever a single song do not have the lyrics we will skip it
-					pass
 
 #search a previous registry of song searched, if there isn't will create a new one
 def checkRegistry(path, filename):
@@ -110,48 +98,52 @@ def checkRegistry(path, filename):
 	if(pref==None):
 		pref = {
 			"alreadySearched": [],
-			"numMP3":0,
-			"numM4A":0
+			"mp3":0,
+			"m4a":0,
+			"flac":0
 		}
 	return pref
 
 
 #process each song by searching and saving the lyrics
-def scanFolder(path):
+def scanFolder(path,isFlush=False):
 	dictSongs = checkRegistry(path, "glyrics.json") #configuration file, stored on @param path
 	for root, directories, files in os.walk(path, topdown=True):
 		for name in files:
-			pathTmp=str(os.path.join(root, name))
-			if(str(doMD5(pathTmp)) not in dictSongs.get("alreadySearched") and name[0]!='.'): #if song is never been searched or hasn't got lyricstxt and file is not a hidden one
-				lyricstxt = searchLyrics(pathTmp)
-				if(pathTmp.endswith(".m4a")):
-					dictSongs["numM4A"]+=1
-					if(lyricstxt!=None and storeLyricsM4A(pathTmp,lyricstxt)): #lyricstxt found, store and save the hash of song (to avoid a rescan)
-						dictSongs.get("alreadySearched").append(doMD5(pathTmp))
+			song_path=str(os.path.join(root, name))
+			if(isFlush):
+				flushLyrics(song_path)
+			elif(str(doMD5(song_path)) not in dictSongs.get("alreadySearched") and name[0]!='.'): #if song is never been searched or hasn't got lyricstxt and file is not a hidden one
+				lyricstxt = searchLyrics(song_path)
+				res_store = storeLyrics(song_path,lyricstxt)
+				if(res_store[0]): ##lyrics found and stored
+					dictSongs.get("alreadySearched").append(doMD5(song_path))
+					try:
+						dictSongs[res_store[1]] += 1
+					except Exception as e:
+						pass
 
-				if(pathTmp.endswith(".flac")):
-					# dictSongs["numFLAC"]+=1 ## TODO: fix it
-					if(lyricstxt!=None and storeLyricsFLAC(pathTmp,lyricstxt)):
-						dictSongs.get("alreadySearched").append(doMD5(pathTmp))
-
-				elif(pathTmp.endswith(".mp3")):
-					dictSongs["numMP3"]+=1
-					if(lyricstxt!=None and storeLyricsMP3(pathTmp,lyricstxt)): #lyricstxt found, store and save the hash of song (to avoid a rescan)
-						dictSongs.get("alreadySearched").append(doMD5(pathTmp))
 
 	return dictSongs
 
 #############################################################################
 #start the program, let choose two option: "Flush" -> erease the previous lyrics, "search" -> scan the folder
 def main():
-	if(len(sys.argv)>1):
-		if(sys.argv[1].upper()=="FLUSH"):
+	if len(sys.argv) < 3:
+		print("Usage: python3 glyrics.py <Flush|Search> <FolderPath>")
+		return
+	
+	command = sys.argv[1].upper()
+	folder_path = sys.argv[2]
+
+	if command == "FLUSH":
+		if (str(input("DANGER:: Insert 'suRe' if you're really sure\n"))=='suRe'):
 			flushLyrics(sys.argv[2])
-		elif(sys.argv[1].upper()=="SEARCH"):
-			dictSongs = scanFolder(sys.argv[2])
-			print(dictSongs)
-			serializeJSON(sys.argv[2],"glyrics.json",dictSongs)
-			print("Done!")
+	elif(sys.argv[1].upper()=="SEARCH"):
+		dictSongs = scanFolder(sys.argv[2])
+		print(dictSongs)
+		serializeJSON(sys.argv[2],"glyrics.json",dictSongs)
+		print("Scan complete!")
 	else:
 		print("Searching for a default configuration...")
 		try:
@@ -163,6 +155,3 @@ def main():
 			print("Try to run with $python3 glyrics.py Flush/Search $Path")
 
 main()
-
-##TODO: beter store function, unified and use a switch to the cases format
-##TODO: review documentations
